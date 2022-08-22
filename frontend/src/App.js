300f233bdd44fb58a3db3e8178895266060c35fa
samiullah <samii51728@gmail.com>
2022-08-22 16:41:13 +0500

    routes are protedted now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
 import Signin from "./pages/signin/Signin";
+import ProtectedRoutes from "./protectedRoutes";
 
 function App() {
   return (
     <div>
-      {/* <Signin /> */}
-      {/* <Register /> */}
-      <Home />
+      <Routes>
+        <Route element={<ProtectedRoutes />}>
+          <Route index path="/" element={<Home />} />
+        </Route>
+        <Route path="register" element={<Register />} />
+        <Route path="signin" element={<Signin />} />
+      </Routes>
+
+      <Register />
     </div>
   );
 }
