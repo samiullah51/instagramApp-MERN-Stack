94478102f7aa4ba775adac0a8151524d17d93982
samiullah <samii51728@gmail.com>
2022-08-22 15:57:49 +0500

    convert to responsive suggestions

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Feed.css";
 import Posts from "./posts/Posts";
 import Stories from "./stories/Stories";
 import { data } from "../../components/feed/stories/data";
 import Suggest from "./suggest/Suggest";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 function Feed() {
+  const [showSuggestions, setShowSuggestions] = useState(true);
   return (
     <div className="feed__container">
       <div className="feed">
@@ -12,7 +15,19 @@ function Feed() {
         <Posts />
       </div>
       {/* Friend Suggestions */}
-      <div className="suggestions">
+
+      {showSuggestions && (
+        <ArrowBackIcon
+          className="showIcon"
+          onClick={() => setShowSuggestions(false)}
+        />
+      )}
+
+      <div className={!showSuggestions ? "suggestions" : "suggestions hide"}>
+        <ArrowForwardIcon
+          className="hideIcon"
+          onClick={() => setShowSuggestions(true)}
+        />
         <div className="suggestion__header">
           <img
             src="https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTc5OTk2ODUyMTMxNzM0ODcy/gettyimages-1229892983-square.jpg"
