2e1a2c3845b0a3578a58926cd563e4e9e06aea6c
samiullah <samii51728@gmail.com>
2022-08-25 22:04:24 +0500

    profile page completed

--- frontend/src/pages/profile/posts/Posts.js
+++ frontend/src/pages/profile/posts/Posts.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { data } from "../../../components/feed/stories/data";
 import "./Posts.css";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 function Posts() {
   return (
     <div className="posts">
       {data.map((user) => (
         <div className="single__post">
           <img src={user.img} />
-          <div className="overlay"></div>
+          <DeleteOutlineIcon />
         </div>
       ))}
     </div>
