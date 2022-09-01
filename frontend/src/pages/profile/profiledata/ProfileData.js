9bbf275ac7ffbeae3810ff3f3cf48a9486470b4c
samiullah <samii51728@gmail.com>
2022-09-01 23:04:05 +0500

    user stroe completed

--- frontend/src/pages/profile/profiledata/ProfileData.js
+++ frontend/src/pages/profile/profiledata/ProfileData.js
@@ -2,15 +2,17 @@ import React from "react";
 import { data } from "../../../components/feed/stories/data";
 import "./ProfileData.css";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import { useSelector } from "react-redux";
 
 function ProfileData() {
+  const user = useSelector((state) => state.user);
   return (
     <div className="profiledata">
       <img src={data[0].img} />
       <div className="right">
         {/* right header */}
         <div className="right__header">
-          <p className="header__username">sami_samiullah51728</p>
+          <p className="header__username">{user.username}</p>
           <button>Edit Profile</button>
           <SettingsOutlinedIcon />
         </div>
