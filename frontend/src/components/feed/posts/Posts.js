61ebd5ce786724dcba5e15f3bb09f595acb2caf0
samiullah <samii51728@gmail.com>
2022-09-02 23:11:04 +0500

    added loading

--- frontend/src/components/feed/posts/Posts.js
+++ frontend/src/components/feed/posts/Posts.js
@@ -5,21 +5,26 @@ import "./Posts.css";
 import axios from "axios";
 function Posts() {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
   // Get all posts
   useEffect(() => {
+    setLoading(true);
     const getPosts = async () => {
       const res = await axios.get("http://localhost:5000/api/post/allposts");
       setPosts(res.data);
       console.log(res.data);
+      setLoading(false);
     };
     getPosts();
   }, []);
 
   return (
     <div>
-      {posts.map((post) => (
-        <Post post={post} />
-      ))}
+      {!loading ? (
+        posts.map((post) => <Post post={post} />)
+      ) : (
+        <h2>Loading...</h2>
+      )}
     </div>
   );
 }
